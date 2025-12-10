fe41802c322a5ef093ad4131ed3511fad1202158
portfolio-team <yamamoto.kotaro.portfolio@gmail.com>
2025-12-10 12:55:24 +0900

    update: refactor & add: comment

--- src/utils/hls_convert.py
+++ src/utils/hls_convert.py
@@ -23,31 +23,31 @@ def cmd_to_aac_hls(input_wav: str, output_dir: str) -> list:
 
 def cmd_to_uncomp_hls(input_wav: str, output_dir: str) -> list:
     """WAVを非圧縮フォーマットでHLS形式に変換"""
-    """セグメントの長さは 2 秒"""
+    """セグメントの長さは 10 秒"""
 
+    allow_sample_rates = [44100, 48000, 88200, 96000, 176400, 192000]
     with wave.open(input_wav, 'rb') as wav_file:
         bits_sample = wav_file.getsampwidth() * 8  # サンプル幅（バイト）をビットに変換
         sample_rate = wav_file.getframerate()
-    if not (sample_rate == 44100 or sample_rate == 48000):
+    if not (sample_rate in allow_sample_rates):
         raise ValueError(f"Unsupported sample rate: {sample_rate}")
 
-    # 例: 先頭10秒は2秒刻み、以降は6秒刻み
     cmd = [
         "ffmpeg",
-        "-i", input_wav,
-        "-ar", str(sample_rate),
-        "-ac", "2",
-        "-f", "segment",
-        "-segment_time", "10",
-        "-segment_format", "wav",
-        "-segment_list", "index.m3u8",
-        "-segment_list_type", "m3u8",
+        "-i", input_wav,  # 入力ファイル
+        "-ar", str(sample_rate),  # 出力音声のサンプリングレート
+        "-ac", "2",  # チャンネル数にステレオを指定
+        "-f", "segment",  # 出力フォーマットに分割出力を指定
+        "-segment_time", "10",  # 1分割の長さ（10 秒ごとに分割）
+        "-segment_format", "wav",  # 分割後ファイルの形式
+        "-segment_list", "index.m3u8",  # インデックスファイル名
+        "-segment_list_type", "m3u8",  # インデックスファイル形式
     ]
 
-    # 24bit PCM の場合、ffmpeg の内部では s24 / s32 フォーマットを使う場合が多いため付与
+    # 音声コーデックの指定
+    # インプット音源に合わせて16bit PCM or 24bit PCMを指定
     if bits_sample == 24:
         cmd.extend(["-c:a", "pcm_s24le"])
-        # cmd.extend(["-sample_fmt", "s32"])
     else:
         cmd.extend(["-c:a", "pcm_s16le"])
 
@@ -61,12 +61,12 @@ def excecute_convert(cmd: list, output_dir: str):
     print(f"[INFO] Running: {' '.join(cmd)}")
     ffmpeg_result = subprocess.run(cmd, capture_output=True, text=True)
     if ffmpeg_result.returncode != 0:
-        print("[ERROR] FFmpeg failed:", result.stderr)
+        print("[ERROR] FFmpeg failed:", ffmpeg_result.stderr)
         sys.exit(1)
 
     mv_result = subprocess.run(["mv", "index.m3u8", output_dir], capture_output=True, text=True)
     if mv_result.returncode != 0:
-        print("[ERROR] mv index.m3u8 failed:", result.stderr)
+        print("[ERROR] mv index.m3u8 failed:", mv_result.stderr)
         sys.exit(1)
 
     print("[INFO] HLS conversion completed.")
